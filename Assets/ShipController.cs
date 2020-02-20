78ebce333bc2fc728e28c9e76ddb5076e9bd4915
EastboundLil <wassarion@gmail.com>
2020-02-20 20:02:51 +0100

    project

---
+++ Assets/ShipController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public float speed = 20f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Ray castPoint = Camera.main.ScreenPointToRay(mouse);
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(castPoint, out hit, Mathf.Infinity))
+            {
+                transform.position = hit.point;
+            }
+        }
+    }
+}
